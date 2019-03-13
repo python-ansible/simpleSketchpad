c7f1cd0c133e193b63d1b98160fe59fa48ae8119
python-ansible <1064534588@qq.com>
2019-03-13 22:17:39 +0800

    添加画笔粗细、颜色，清空画布，下载等功能

--- main.js
+++ main.js
@@ -1,27 +1,102 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var lineWidth = 5
 
 autoGetSize(canvas)
+
+//设置画布背景
+ctx.fillStyle = 'white';
+ctx.fillRect(0,0, canvas.width, canvas.height)
+
 listenToUser(canvas)
 
 //eraserEnabled
 var eraserEnabled = false
+// eraser.onclick = function () {
+//     eraserEnabled = true
+//     actions.className = 'actions x'
+// }
+// brush.onclick = function () {
+//     eraserEnabled = false
+//     actions.className = 'actions'
+// }
 eraser.onclick = function () {
+    // console.log('eraser')
     eraserEnabled = true
-    actions.className = 'actions x'
+    eraser.classList.add('active')
+    pen.classList.remove('active')
 }
-brush.onclick = function () {
+pen.onclick = function () {
+    // console.log('pen')
     eraserEnabled = false
-    actions.className = 'actions'
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+}
+
+//改变画笔颜色
+black.onclick = function () {
+    ctx.fillStyle = 'black'
+    ctx.strokeStyle = 'black'
+    black.classList.add('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+red.onclick = function () {
+    ctx.fillStyle = 'red'
+    ctx.strokeStyle = 'red'
+    black.classList.remove('active')
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+green.onclick = function () {
+    ctx.fillStyle = 'green'
+    ctx.strokeStyle = 'green'
+    black.classList.remove('active')
+    red.classList.remove('active')
+    green.classList.add('active')
+    blue.classList.remove('active')
+}
+blue.onclick = function () {
+    ctx.fillStyle = 'blue'
+    ctx.strokeStyle = 'blue'
+    black.classList.remove('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.add('active')
+}
+thin.onclick = function () {
+    thin.classList.add('active')
+    thick.classList.remove('active')
+    lineWidth = 5
+}
+thick.onclick = function () {
+    thin.classList.remove('active')
+    thick.classList.add('active')
+    lineWidth = 10
+}
+clean.onclick = function () {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+}
+save.onclick = function () {
+    var url = canvas.toDataURL("image/png")
+    // var url = canvas.toDataURL("image/jpeg")
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = 'myPicture'
+    a.target = '_blank'
+    a.click()
 }
 
 //drawLine
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
-    ctx.strokeStyle = 'black';
     ctx.moveTo(x1, y1)
-    ctx.lineWidth = 5
     ctx.lineTo(x2, y2)
+    ctx.lineWidth = lineWidth
+    // ctx.lineJoin = "round"; //圆角
     ctx.stroke()
     ctx.closePath()
 }
@@ -65,15 +140,16 @@ function listenToUser(canvas) {
         canvas.ontouchmove = function (a) {
             var x = a.touches[0].clientX
             var y = a.touches[0].clientY
-            if (using) {
-                if (eraserEnabled) {
-                    ctx.clearRect(x - 5, y - 5, 10, 10)
-                } else {
-                    var newPoint = {"x": x, "y": y}
-                    // drawCircle(x, y, 1)
-                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-                    lastPoint = newPoint
-                }
+            if (!using) {
+                return
+            }
+            if (eraserEnabled) {
+                ctx.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = {"x": x, "y": y}
+                // drawCircle(x, y, 1)
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint
             }
         }
 
@@ -83,6 +159,7 @@ function listenToUser(canvas) {
     } else {
         //非触屏设备
         canvas.onmousedown = function (a) {
+            // console.log('down')
             var x = a.clientX
             var y = a.clientY
             using = true
@@ -95,21 +172,24 @@ function listenToUser(canvas) {
         }
 
         canvas.onmousemove = function (a) {
+            // console.log('move')
             var x = a.clientX
             var y = a.clientY
-            if (using) {
-                if (eraserEnabled) {
-                    ctx.clearRect(x - 5, y - 5, 10, 10)
-                } else {
-                    var newPoint = {"x": x, "y": y}
-                    // drawCircle(x, y, 1)
-                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-                    lastPoint = newPoint
-                }
+            if (!using) {
+                return
+            }
+            if (eraserEnabled) {
+                ctx.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = {"x": x, "y": y}
+                // drawCircle(x, y, 1)
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint
             }
         }
 
         canvas.onmouseup = function (a) {
+            // console.log('up')
             using = false
         }
     }
